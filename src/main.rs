4b7a3caf0ee11790e354022ab6a2b3752207dd4d
Justin Meimar <j.meimar111@gmail.com>
2023-05-14 13:51:53 -0600

    echo working

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,108 @@
 use serde::{Serialize, Deserialize};
 use serde_json::Deserializer;
-use std::io::BufReader;
+use std::io::{BufReader, BufWriter, Write, Stdout};
 
-struct Node {
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Message<T> {
+    src: String, 
+    dest: String,
+    body: Body<T>, 
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Body<T> {
+    msg_id: Option<usize>,
+    #[serde(flatten)]
+    payload: T
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum Payload {
+    Init(InitData),
+    InitOk,
+    Echo(EchoData),
+    EchoOk,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct EchoData {
+    echo: String
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct InitData {
+    node_id: String,
+    node_ids: Vec<String>
+}
+
+struct InitNode {
     id: usize
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct Message {
-    src: String, 
-    dst: String,
-    body: Body, 
+trait Available<T> {
+    fn respond(&self, msg: &Message<T>, stdout: &mut BufWriter<Stdout>) -> std::io::Result<()>;
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "snake_case")]
-enum Body {
-    Echo(EchoBody),
-    Broadcast(BroadcastBody),
+impl Available<Payload> for InitNode {
+    fn respond(&self, msg: &Message<Payload>, stdout: &mut BufWriter<Stdout>) -> std::io::Result<()> {
+
+        let reply : Message<Payload> = Message {
+            src: msg.dest.clone(),
+            dest: msg.src.clone(),
+            body: Body {
+                msg_id: Some(self.id),
+                payload: Payload::InitOk
+            }
+        };
+
+        let reply_str = serde_json::to_string(&reply);
+
+        match reply_str {
+            Ok(s) => {
+                stdout.write_all(s.as_bytes())?;
+                stdout.flush()?;
+            }
+            Err(e) => {
+                println!("Error in writting the reply to stdout!\n, {:?}", e);
+            }
+        }
+        // match msg.body.payload 
+
+        Ok(())
+    }
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct EchoBody {
-    #[serde(rename="type")]
-    ty: String,
-    msg: String
-}
+fn main() -> std::io::Result<()> {
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct BroadcastBody {
-    #[serde(rename="type")]
-    ty: String,
-    msg: String, 
-    timeout: String
-}
-
-fn main() {
-
-    let stdin = std::io::stdin();
+    let stdout = std::io::stdout();
+    let mut writer = BufWriter::new(stdout);
+    let stdin = std::io::stdin(); 
     let reader = BufReader::new(stdin);
-    let stream = Deserializer::from_reader(reader).into_iter::<Message>();
-    
+    let stream = Deserializer::from_reader(reader).into_iter::<Message<Payload>>();
+
+    let init_node = InitNode {
+        id : 1
+    };
+
     for msg in stream {
         match msg {
             Ok(m) => {
-                println!("Deserialization Success! {}", m.src.to_string());
+                match m.body.payload {
+                    Payload::Init(_) => {
+                        init_node.respond(&m, &mut writer)?;
+                    },
+                    Payload::InitOk => {
+                        println!("Hello, InitOk")
+                    },
+                    Payload::Echo(_) => {
+                        init_node.respond(&m, &mut writer)?;
+                    },
+                    Payload::EchoOk => {
+                        println!("Hello, EchoOk")
+                    }
+                }     
             } 
             Err(e) => {
                 println!("Deserialization Error! {:?}", e);
@@ -52,4 +110,23 @@ fn main() {
         }
     }
 
+    let m : Message<Payload> = Message {
+        src: "source".to_string(),
+        dest: "destination".to_string(),
+        body: Body {
+            msg_id: Some(1),
+            payload: Payload::Init(
+                InitData {
+                    node_id: "n1".to_string(),
+                    node_ids: vec!["n1".to_string(), "n2".to_string()],
+                }
+            ),
+        },
+    };
+
+    let s = serde_json::to_string_pretty(&m)?;
+
+    // println!("{}", s);
+
+    Ok(())
 }
